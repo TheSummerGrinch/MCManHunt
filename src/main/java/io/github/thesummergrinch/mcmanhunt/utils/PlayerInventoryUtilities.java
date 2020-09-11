aa848b9b302520e9be31f97ba62cb7da336ef185
TheSummerGrinch <66258844+TheSummerGrinch@users.noreply.github.com>
2020-09-11 15:08:24 +0200

    Fixed spelling error.

--- src/main/java/io/github/thesummergrinch/mcmanhunt/utils/PlayerInventoryUtilities.java
+++ src/main/java/io/github/thesummergrinch/mcmanhunt/utils/PlayerInventoryUtilities.java
@@ -9,7 +9,7 @@ import org.bukkit.inventory.meta.CompassMeta;
 public final class PlayerInventoryUtilities {
 
     /**
-     * Clears the inverntory of the given Player.
+     * Clears the inventory of the given Player.
      * @param player - Player whose inventory will be cleared.
      */
     public static void clearPlayerInventory(final Player player) {
