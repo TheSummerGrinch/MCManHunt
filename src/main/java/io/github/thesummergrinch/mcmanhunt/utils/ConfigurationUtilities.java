358faddfa0bbf94453e47b07b85e4c5cdc331bcf
TheSummerGrinch <66258844+TheSummerGrinch@users.noreply.github.com>
2020-09-15 21:30:54 +0200

    Reworked Config operations.
    Now capable of saving an ongoing game to the config.yml, and reading said data to resume the game.

--- src/main/java/io/github/thesummergrinch/mcmanhunt/utils/ConfigurationUtilities.java
+++ src/main/java/io/github/thesummergrinch/mcmanhunt/utils/ConfigurationUtilities.java
@@ -1,9 +1,12 @@
 package io.github.thesummergrinch.mcmanhunt.utils;
 
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.UUID;
 import java.util.logging.Level;
 
 public final class ConfigurationUtilities {
@@ -12,41 +15,14 @@ public final class ConfigurationUtilities {
     private static FileConfiguration fileConfiguration;
 
     /**
-     * Loads - or reload - FileConfiguration from the config.yml file in the plugin's datafolder.
-     *
-     * @param forcedUpdate - If true, the config.yml will be reloaded and re-cached. If false, the current cached version will be returned.
-     * @return The FileConfiguration stored in the fileConfiguration-variable or loaded from the config.yml file.
-     */
-    public static FileConfiguration getConfiguration(Plugin plugin, boolean forcedUpdate) {
-        if (!new File(plugin.getDataFolder(), "config.yml").exists()) {
-            generateDefaultConfiguration();
-            ManHuntUtilities.setFirstRun(true);
-            plugin.getLogger().log(Level.INFO, "A config.yml was automatically " +
-                    "generated.");
-            fileConfiguration = plugin.getConfig();
-        }
-        if (fileConfiguration == null || forcedUpdate) {
-            fileConfiguration = plugin.getConfig();
-        }
-        return fileConfiguration;
-    }
-
-    /**
-     * Generates the default config.yml file, in the plugin's datafolder.
-     */
-    private static void generateDefaultConfiguration() {
-        ManHuntUtilities.getManHuntPlugin().saveDefaultConfig();
-    }
-
-    /**
      * Gets the int-value associated with the given path in the config.yml file.
      *
      * @param path - Path of the requested Integer.
      * @return int - The int associated with the given path.
      */
-    public static int getInt(Plugin plugin, final String path) {
-        if (fileConfiguration == null) getConfiguration(plugin, true);
-        return getConfiguration(plugin, false).getInt(path);
+    static int getInt(Plugin plugin, final String path) {
+        if (fileConfiguration == null) loadConfig(plugin);
+        return fileConfiguration.getInt(path);
     }
 
     /**
@@ -56,12 +32,57 @@ public final class ConfigurationUtilities {
      * @return int - The Integer associated with the given path.
      */
     public static boolean getBoolean(Plugin plugin, final String path) {
-        if (fileConfiguration == null) getConfiguration(plugin, true);
-        return getConfiguration(plugin, false).getBoolean(path);
+        if (fileConfiguration == null) loadConfig(plugin);
+        return fileConfiguration.getBoolean(path);
     }
 
-    /*public static boolean saveOngoingGame() {
-        fileConfiguration.set("", "");
-    }*/
+    private static void saveOngoingGame() {
+        final ArrayList<Player> hunters = new ArrayList<>(ManHuntUtilities.getHunters());
+        final ArrayList<Player> runners = new ArrayList<>(ManHuntUtilities.getRunners());
+        final ArrayList<String> hunterUUIDs = new ArrayList<>();
+        final ArrayList<String> runnerUUIDs = new ArrayList<>();
+        for (Player hunter : hunters) {
+            hunterUUIDs.add(hunter.getUniqueId().toString());
+        }
+        for (Player runner : runners) {
+            runnerUUIDs.add(runner.getUniqueId().toString());
+        }
+        fileConfiguration.set("game-ongoing", true);
+        fileConfiguration.set("current-hunters", hunterUUIDs);
+        fileConfiguration.set("current-runners", runnerUUIDs);
+    }
+
+    public static void saveConfig(final Plugin plugin) {
+        fileConfiguration.set("max-runners", ManHuntUtilities.getMaxRunners());
+        fileConfiguration.set("max-hunters", ManHuntUtilities.getMaxHunters());
+        if (GameFlowUtilities.isGameInProgress()) saveOngoingGame();
+        plugin.saveConfig();
+    }
+
+    public static void loadConfig(final Plugin plugin) {
+        if (!new File(plugin.getDataFolder(), "config.yml").exists()) {
+            plugin.saveDefaultConfig();
+            plugin.getLogger().log(Level.CONFIG, "No config.yml detected. Automatically generated the default " +
+                    "config.yml.");
+            ManHuntUtilities.setFirstRun();
+        }
+        fileConfiguration = plugin.getConfig();
+        ManHuntUtilities.setMaxRunners(fileConfiguration.getInt("max-runners"));
+        ManHuntUtilities.setMaxHunters(fileConfiguration.getInt("max-hunters"));
+        if (fileConfiguration.getBoolean("game-ongoing")) {
+            GameFlowUtilities.setGameInProgress(true);
+            GameFlowUtilities.pauseGame();
+            final ArrayList<String> hunterUUIDStrings = (ArrayList<String>) fileConfiguration
+                    .getObject("current-hunters", ArrayList.class);
+            final ArrayList<String> runnerUUIDStrings = (ArrayList<String>) fileConfiguration
+                    .getObject("current-runners", ArrayList.class);
+            for (String hunterUUIDString : hunterUUIDStrings) {
+                ManHuntUtilities.addSavedGameHunter(UUID.fromString(hunterUUIDString));
+            }
+            for (String runnerUUIDString : runnerUUIDStrings) {
+                ManHuntUtilities.addSavedGameRunner(UUID.fromString(runnerUUIDString));
+            }
+        }
+    }
 
 }
