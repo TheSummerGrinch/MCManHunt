c20dd5c90c23f9188d29dcb07e983fccba50067c
TheSummerGrinch <66258844+TheSummerGrinch@users.noreply.github.com>
2020-09-15 21:27:16 +0200

    Refactored OnPlayerLoginEventHandler to OnPlayerJoinEventHandler.
    Some operations cannot be performed using the PlayerLoginEvent, so it was replaced with PlayerJoinEvent.

--- src/main/java/io/github/thesummergrinch/mcmanhunt/eventhandlers/OnPlayerJoinEventHandler.java
+++ src/main/java/io/github/thesummergrinch/mcmanhunt/eventhandlers/OnPlayerJoinEventHandler.java
@@ -2,30 +2,30 @@ package io.github.thesummergrinch.mcmanhunt.eventhandlers;
 
 import io.github.thesummergrinch.mcmanhunt.utils.GameFlowUtilities;
 import io.github.thesummergrinch.mcmanhunt.utils.ManHuntUtilities;
-import io.github.thesummergrinch.mcmanhunt.utils.PlayerInventoryUtilities;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerLoginEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
 
 import java.util.UUID;
 
-public class OnPlayerLoginEventHandler implements Listener {
+public class OnPlayerJoinEventHandler implements Listener {
 
     /**
      * When a player logs in, check whether or not a game is ongoing. If not, and the player is currently in a team,
-     * reset the player-roles and clear the player's inventory.
+     * reset the player-roles.
      *
      * @param event - The PlayerLoginEvent passed by the server.
      */
-    @EventHandler
-    public void onPlayerLoginEvent(final PlayerLoginEvent event) {
+    @EventHandler(priority = EventPriority.HIGH)
+    public void onPlayerJoinEvent(final PlayerJoinEvent event) {
         final Player player = event.getPlayer();
         final UUID playerUUID = player.getUniqueId();
         if (!GameFlowUtilities.isGameInProgress()) {
             if (ManHuntUtilities.isRunner(playerUUID) || ManHuntUtilities.isHunter(playerUUID)) {
                 ManHuntUtilities.resetplayerroles();
-                PlayerInventoryUtilities.clearPlayerInventory(player);
+            }
             }
         }
     }
