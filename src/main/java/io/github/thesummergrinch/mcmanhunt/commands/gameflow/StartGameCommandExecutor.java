ba6c5a56f7af99e5c07eddee0c67ed84ccc76944
TheSummerGrinch <66258844+TheSummerGrinch@users.noreply.github.com>
2020-09-25 20:49:37 +0200

    Fixed the Random-team feature

--- src/main/java/io/github/thesummergrinch/mcmanhunt/commands/gameflow/StartGameCommandExecutor.java
+++ src/main/java/io/github/thesummergrinch/mcmanhunt/commands/gameflow/StartGameCommandExecutor.java
@@ -9,11 +9,18 @@ import org.bukkit.command.CommandSender;
 public class StartGameCommandExecutor implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (sender.isOp() && GameController.getInstance().getGameState().equals(GameController.GameState.DEFAULT)) {
-            if (UserCache.getInstance().getNumberOfHunters() == 0L
-                    || UserCache.getInstance().getNumberOfRunners() == 0L) {
-                sender.sendMessage("Assign players to both teams before starting!");
-                return true;
+        if (sender.isOp()) {
+            if (GameController.getInstance().getGameState().equals(GameController.GameState.DEFAULT)) {
+                if (GameController.getInstance().getManHuntGameMode().equals(GameController.GameMode.RANDOM)
+                        && UserCache.getInstance().getPlayersToRandomlyAllocate().size() >= 2) {
+                    GameController.getInstance().startGame();
+                    return true;
+                }
+                if (UserCache.getInstance().getNumberOfHunters() == 0L
+                        || UserCache.getInstance().getNumberOfRunners() == 0L) {
+                    sender.sendMessage("Assign players to both teams before starting!");
+                    return true;
+                }
             }
             GameController.getInstance().startGame();
             return true;
