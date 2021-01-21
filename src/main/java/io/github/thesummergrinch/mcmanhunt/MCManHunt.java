56e10c0e99411df7e2a7cf43ab6a2ddf1509bdb2
TheSummerGrinch <66258844+TheSummerGrinch@users.noreply.github.com>
2021-01-21 14:54:33 +0100

    Update MCManHunt.java
    Most significant change is disabling Metrics for now.

--- src/main/java/io/github/thesummergrinch/mcmanhunt/MCManHunt.java
+++ src/main/java/io/github/thesummergrinch/mcmanhunt/MCManHunt.java
@@ -1,67 +1,79 @@
 package io.github.thesummergrinch.mcmanhunt;
 
-import io.github.thesummergrinch.mcmanhunt.commands.config.SetForceRandomTeamCommandExecutor;
-import io.github.thesummergrinch.mcmanhunt.commands.config.SetMaxHuntersCommandExecutor;
-import io.github.thesummergrinch.mcmanhunt.commands.config.SetMaxRunnersCommandExecutor;
-import io.github.thesummergrinch.mcmanhunt.commands.gameflow.PauseGameCommandExecutor;
-import io.github.thesummergrinch.mcmanhunt.commands.gameflow.ResumeGameCommandExecutor;
-import io.github.thesummergrinch.mcmanhunt.commands.gameflow.StartGameCommandExecutor;
-import io.github.thesummergrinch.mcmanhunt.commands.gameflow.StopGameCommandExecutor;
-import io.github.thesummergrinch.mcmanhunt.commands.player.admin.AddHunterCommandExecutor;
-import io.github.thesummergrinch.mcmanhunt.commands.player.admin.AddRunnerCommandExecutor;
-import io.github.thesummergrinch.mcmanhunt.commands.player.info.ListHuntersCommandExecutor;
-import io.github.thesummergrinch.mcmanhunt.commands.player.info.ListRunnersCommandExecutor;
-import io.github.thesummergrinch.mcmanhunt.commands.player.team.JoinHuntersCommandExecutor;
-import io.github.thesummergrinch.mcmanhunt.commands.player.team.JoinRandomTeamCommandExecutor;
-import io.github.thesummergrinch.mcmanhunt.commands.player.team.JoinRunnersCommandExecutor;
-import io.github.thesummergrinch.mcmanhunt.commands.player.team.LeaveTeamCommandExecutor;
+import io.github.thesummergrinch.mcmanhunt.cache.GameCache;
+import io.github.thesummergrinch.mcmanhunt.cache.UniverseCache;
+import io.github.thesummergrinch.mcmanhunt.commands.game.info.ListGamesCommandExecutor;
+import io.github.thesummergrinch.mcmanhunt.commands.game.info.ListHuntersCommandExecutor;
+import io.github.thesummergrinch.mcmanhunt.commands.game.info.ListRunnersCommandExecutor;
+import io.github.thesummergrinch.mcmanhunt.commands.game.op.gameflow.*;
+import io.github.thesummergrinch.mcmanhunt.commands.game.op.universe.DestroyUniverseCommandExecutor;
+import io.github.thesummergrinch.mcmanhunt.commands.game.op.universe.SetDestroyUniverseOnStopCommandExecutor;
+import io.github.thesummergrinch.mcmanhunt.commands.game.player.JoinGameCommandExecutor;
+import io.github.thesummergrinch.mcmanhunt.commands.game.player.JoinTeamCommandExecutor;
+import io.github.thesummergrinch.mcmanhunt.commands.plugin.info.ManHuntVersionCommandExecutor;
 import io.github.thesummergrinch.mcmanhunt.eventhandlers.*;
+import io.github.thesummergrinch.mcmanhunt.events.ManHuntWinEvent;
+import io.github.thesummergrinch.mcmanhunt.game.Game;
+import io.github.thesummergrinch.mcmanhunt.game.players.PlayerState;
 import io.github.thesummergrinch.mcmanhunt.io.FileConfigurationLoader;
+import io.github.thesummergrinch.mcmanhunt.universe.Universe;
 import org.bstats.bukkit.Metrics;
+import org.bukkit.configuration.serialization.ConfigurationSerialization;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.logging.Level;
 
 public final class MCManHunt extends JavaPlugin {
 
+    static {
+        ConfigurationSerialization.registerClass(PlayerState.class);
+        ConfigurationSerialization.registerClass(Game.class);
+        ConfigurationSerialization.registerClass(Universe.class);
+        ConfigurationSerialization.registerClass(GameCache.class);
+    }
+
+
     @Override
     public void onEnable() {
-        registerEvents();
-        registerCommandExecutors();
-        if (FileConfigurationLoader.getInstance().getBoolean("allow-metrics")) enableMetrics();
+        // Plugin startup logic
+        registerEventHandlers();
+        registerCommands();
+        GameCache.getInstance().getGameCacheFromSave();
     }
 
     @Override
     public void onDisable() {
-        FileConfigurationLoader.getInstance().saveConfig();
+        UniverseCache.getInstance().onDisable();
+        FileConfigurationLoader.getInstance().saveGames();
     }
 
-    private void registerCommandExecutors() {
-        this.getCommand("setmaxhunters").setExecutor(new SetMaxHuntersCommandExecutor());
-        this.getCommand("setmaxrunners").setExecutor(new SetMaxRunnersCommandExecutor());
-        this.getCommand("pausegame").setExecutor(new PauseGameCommandExecutor());
+    private void registerCommands() {
+        this.getCommand("initializegame").setExecutor(new InitializeGameCommandExecutor());
+        this.getCommand("joingame").setExecutor(new JoinGameCommandExecutor());
         this.getCommand("startgame").setExecutor(new StartGameCommandExecutor());
-        this.getCommand("stopgame").setExecutor(new StopGameCommandExecutor());
-        this.getCommand("resumegame").setExecutor(new ResumeGameCommandExecutor());
-        this.getCommand("addhunter").setExecutor(new AddHunterCommandExecutor());
-        this.getCommand("addrunner").setExecutor(new AddRunnerCommandExecutor());
+        this.getCommand("jointeam").setExecutor(new JoinTeamCommandExecutor());
         this.getCommand("listhunters").setExecutor(new ListHuntersCommandExecutor());
         this.getCommand("listrunners").setExecutor(new ListRunnersCommandExecutor());
-        this.getCommand("joinhunters").setExecutor(new JoinHuntersCommandExecutor());
-        this.getCommand("joinrunners").setExecutor(new JoinRunnersCommandExecutor());
-        this.getCommand("leaveteam").setExecutor(new LeaveTeamCommandExecutor());
-        this.getCommand("setforcerandomteam").setExecutor(new SetForceRandomTeamCommandExecutor());
-        this.getCommand("joinrandomteam").setExecutor(new JoinRandomTeamCommandExecutor());
+        this.getCommand("listgames").setExecutor(new ListGamesCommandExecutor());
+        this.getCommand("pausegame").setExecutor(new PauseGameCommandExecutor());
+        this.getCommand("resumegame").setExecutor(new ResumeGameCommandExecutor());
+        this.getCommand("stopgame").setExecutor(new StopGameCommandExecutor());
+        this.getCommand("destroyuniverse").setExecutor(new DestroyUniverseCommandExecutor());
+        this.getCommand("setdestroyuniverseonstop").setExecutor(new SetDestroyUniverseOnStopCommandExecutor());
+        this.getCommand("manhuntversion").setExecutor(new ManHuntVersionCommandExecutor());
     }
 
-    private void registerEvents() {
-        this.getServer().getPluginManager().registerEvents(new OnPlayerMoveEventHandler(), this);
-        this.getServer().getPluginManager().registerEvents(new OnCompassInteractEventHandler(), this);
-        this.getServer().getPluginManager().registerEvents(new OnPlayerJoinEventHandler(), this);
-        this.getServer().getPluginManager().registerEvents(new OnPlayerDeathEventHandler(), this);
+    private void registerEventHandlers() {
         this.getServer().getPluginManager().registerEvents(new OnBlockDamageEventHandler(), this);
-        this.getServer().getPluginManager().registerEvents(new OnPlayerRespawnEventHandler(), this);
+        this.getServer().getPluginManager().registerEvents(new OnEnderDragonDeathEventHandler(), this);
+        this.getServer().getPluginManager().registerEvents(new OnManHuntWinEventHandler(), this);
+        this.getServer().getPluginManager().registerEvents(new OnPlayerDamagedEventHandler(), this);
+        this.getServer().getPluginManager().registerEvents(new OnPlayerDeathEventHandler(), this);
+        this.getServer().getPluginManager().registerEvents(new OnPlayerInteractEventHandler(), this);
+        this.getServer().getPluginManager().registerEvents(new OnPlayerJoinEventHandler(), this);
+        this.getServer().getPluginManager().registerEvents(new OnPlayerMoveEventHandler(), this);
         this.getServer().getPluginManager().registerEvents(new OnPlayerPortalEventHandler(), this);
+        this.getServer().getPluginManager().registerEvents(new OnPlayerRespawnEventHandler(), this);
     }
 
     private void enableMetrics() {
