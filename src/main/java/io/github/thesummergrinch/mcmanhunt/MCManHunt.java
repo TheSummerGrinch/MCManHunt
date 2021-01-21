4e86b39f885f6f6c4e3d49c2ed863b90cbd83c76
TheSummerGrinch <66258844+TheSummerGrinch@users.noreply.github.com>
2021-01-21 15:58:48 +0100

    Update MCManHunt.java
    Added the MCManHuntStringCache class to registered serializable classes.

--- src/main/java/io/github/thesummergrinch/mcmanhunt/MCManHunt.java
+++ src/main/java/io/github/thesummergrinch/mcmanhunt/MCManHunt.java
@@ -1,6 +1,7 @@
 package io.github.thesummergrinch.mcmanhunt;
 
 import io.github.thesummergrinch.mcmanhunt.cache.GameCache;
+import io.github.thesummergrinch.mcmanhunt.cache.MCManHuntStringCache;
 import io.github.thesummergrinch.mcmanhunt.cache.UniverseCache;
 import io.github.thesummergrinch.mcmanhunt.commands.game.info.ListGamesCommandExecutor;
 import io.github.thesummergrinch.mcmanhunt.commands.game.info.ListHuntersCommandExecutor;
@@ -30,6 +31,7 @@ public final class MCManHunt extends JavaPlugin {
         ConfigurationSerialization.registerClass(Game.class);
         ConfigurationSerialization.registerClass(Universe.class);
         ConfigurationSerialization.registerClass(GameCache.class);
+        ConfigurationSerialization.registerClass(MCManHuntStringCache.class);
     }
 
 
