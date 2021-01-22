4d9ebacf21fa766bdc462a2d42bf07ef5a4ad3e4
TheSummerGrinch <66258844+TheSummerGrinch@users.noreply.github.com>
2021-01-22 14:55:05 +0100

    Added documentation to Universe-class

--- src/main/java/io/github/thesummergrinch/mcmanhunt/universe/Universe.java
+++ src/main/java/io/github/thesummergrinch/mcmanhunt/universe/Universe.java
@@ -15,13 +15,25 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.logging.Level;
 
+/**
+ * Wrapper class used to hold and interact with the three {@link org.bukkit.World}-objects necessary to construct an
+ * MCManHunt-game.
+ */
 public final class Universe implements ConfigurationSerializable {
 
+    // The Map<String, World> that effectively functions as the cache.
     private final HashMap<String, World> worldHashMap;
 
+    // Identifier used to register the Universe-object in the UniverseCache.
     private final String universeName;
 
+    // Toggleable flag used to denote whether or not the Universe, and the related persistent world-files should be
+    // deleted once the game ends (naturally or through commands). True by default, unless the game will be played in
+    // the default world.
     private boolean destroyWhenGameIsStopped;
+
+    // Flag used to determine whether or not the persistent world-files of this Universe should be deleted when the
+    // server is (gracefully) stopped.
     private boolean markedForDestruction;
 
     public Universe(final String universeName) {
@@ -33,12 +45,22 @@ public final class Universe implements ConfigurationSerializable {
         this.markedForDestruction = false;
     }
 
+    /**
+     * Method solely used to deserialize a yml-representation of a {@link Universe}, as specified in the
+     * {@link #serialize}-method.
+     * @param objects - Map of key-value pairs, representing the {@link Universe}-object.
+     * @return universe - a reconstruction of the saved {@link Universe}.
+     */
+    @SuppressWarnings("unused")
     public static @NotNull Universe deserialize(Map<String, Object> objects) {
         Universe universe = new Universe((String) objects.get("name"));
         universe.setDestroyWhenGameIsStopped((Boolean) objects.get("destroy-when-game-is-stopped"));
         return universe;
     }
 
+    /**
+     * Generates an Overworld, a Nether and an End dimension, and stores them in the {@link HashMap}.
+     */
     private void populateUniverse() {
         final World overworld = WorldCreator.name(universeName).environment(World.Environment.NORMAL)
                 .generateStructures(true).type(WorldType.NORMAL).createWorld();
@@ -51,11 +73,20 @@ public final class Universe implements ConfigurationSerializable {
         this.worldHashMap.put(end.getName(), end);
     }
 
+    /**
+     * Gets the {@link org.bukkit.World} stored with the given key.
+     * @param worldName - key to the {@link org.bukkit.World}-object.
+     * @return {@link org.bukkit.World}
+     */
     @Nullable
     public World getWorld(@NotNull final String worldName) {
         return worldHashMap.get(worldName);
     }
 
+    /**
+     * Serializes the {@link Universe}-object into a yml-representation.
+     * @return {@link Map} containing key-value pairs representing the {@link Universe}.
+     */
     @Override
     public @NotNull Map<String, Object> serialize() {
         HashMap<String, Object> objects = new HashMap<>();
@@ -64,14 +95,26 @@ public final class Universe implements ConfigurationSerializable {
         return objects;
     }
 
+    /**
+     * Gets the name of the {@link Universe}.
+     * @return
+     */
     public @NotNull String getName() {
         return this.universeName;
     }
 
+    /**
+     * Sets the {@link Difficulty} for each of the {@link World}-objects stored in the {@link Universe}.
+     * @param difficulty - target difficulty
+     */
     public void setDifficulty(@NotNull final Difficulty difficulty) {
         this.worldHashMap.values().forEach(world -> world.setDifficulty(difficulty));
     }
 
+    /**
+     * Unloads all the {@link World}-objects stored in this {@link Universe}, and deletes the corresponding persistent
+     * world-files.
+     */
     public void destroyUniverse() {
         this.worldHashMap.forEach((worldName, world) -> {
             Bukkit.unloadWorld(worldName, false);
@@ -86,6 +129,9 @@ public final class Universe implements ConfigurationSerializable {
         });
     }
 
+    /**
+     * @return boolean - whether or not the {@link Universe} should be permanently deleted when the game ends.
+     */
     public boolean getDestroyWhenGameIsStopped() {
         return this.destroyWhenGameIsStopped;
     }
@@ -102,6 +148,14 @@ public final class Universe implements ConfigurationSerializable {
         this.markedForDestruction = true;
     }
 
+    /**
+     * Sets the specified {@link GameRule} to the specified value. E.g.
+     * {@code setGameRule(GameRule.DO_DAYLIGHT_CYCLE, false)} disables the daylight-cycle of the Worlds stored in this
+     * {@link Universe}.
+     * @param gameRule - The {@link GameRule} to be changed/set.
+     * @param value - The new value.
+     * @param <T> - Generic type
+     */
     public <T> void setGameRule(GameRule<T> gameRule, T value) {
         this.worldHashMap.values().forEach(world -> world.setGameRule(gameRule, value));
     }
