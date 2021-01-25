187c7aaa75e6c8e9dcfd17b745ff3c5d0083e916
TheSummerGrinch <66258844+TheSummerGrinch@users.noreply.github.com>
2021-01-25 23:43:23 +0100

    Create ManHuntStringNotFoundException.java
    Exception in case a string cannot be found in the string cache and the default string cache.

---
+++ src/main/java/io/github/thesummergrinch/mcmanhunt/exceptions/ManHuntStringNotFoundException.java
@@ -0,0 +1,9 @@
+package io.github.thesummergrinch.mcmanhunt.exceptions;
+
+public class ManHuntStringNotFoundException extends Exception {
+
+    public ManHuntStringNotFoundException(final String message) {
+        super(message);
+    }
+
+}
