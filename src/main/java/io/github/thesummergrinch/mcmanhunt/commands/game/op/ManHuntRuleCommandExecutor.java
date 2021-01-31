1d298011fe8874aeee000f187df32b53470c7cd3
TheSummerGrinch <66258844+TheSummerGrinch@users.noreply.github.com>
2021-01-31 18:36:52 +0100

    Implementing TabCompleter
    Implemented a TabCompleter in ManHuntRuleCommandExecutor, allowing admins to easily choose which rule to change.

--- src/main/java/io/github/thesummergrinch/mcmanhunt/commands/game/op/ManHuntRuleCommandExecutor.java
+++ src/main/java/io/github/thesummergrinch/mcmanhunt/commands/game/op/ManHuntRuleCommandExecutor.java
@@ -6,9 +6,23 @@ import io.github.thesummergrinch.mcmanhunt.io.lang.LanguageFileLoader;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
-public class ManHuntRuleCommandExecutor implements CommandExecutor {
+import java.util.ArrayList;
+import java.util.List;
+
+public class ManHuntRuleCommandExecutor implements CommandExecutor, TabCompleter {
+
+    private static List<String> AVAILABLE_MANHUNT_RULES = new ArrayList<String>() {
+        {
+            add("compass-enabled-in-nether");
+            add("player-roles-randomized");
+            add("headstart");
+        }
+    };
+
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
         if (sender.isOp() && args.length >= 3) {
@@ -21,5 +35,10 @@ public class ManHuntRuleCommandExecutor implements CommandExecutor {
             return true;
         }
         return false;
+    }
+
+    @Override
+    public @Nullable List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String alias, @NotNull String[] args) {
+        return ManHuntRuleCommandExecutor.AVAILABLE_MANHUNT_RULES;
     }
 }
