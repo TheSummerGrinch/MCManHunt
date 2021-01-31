1aa4dff3e0a5cadb9a9d74b89d5bcab4f96cc24a
TheSummerGrinch <66258844+TheSummerGrinch@users.noreply.github.com>
2021-01-31 18:39:22 +0100

    Implementing TabCompleter
    Implemented a TabCompleter to allow players to easily join a game from the list of initialized games.

--- src/main/java/io/github/thesummergrinch/mcmanhunt/commands/game/player/JoinGameCommandExecutor.java
+++ src/main/java/io/github/thesummergrinch/mcmanhunt/commands/game/player/JoinGameCommandExecutor.java
@@ -6,12 +6,16 @@ import io.github.thesummergrinch.mcmanhunt.io.lang.LanguageFileLoader;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
+import java.util.List;
 import java.util.UUID;
 
-public class JoinGameCommandExecutor implements CommandExecutor {
+public class JoinGameCommandExecutor implements CommandExecutor, TabCompleter {
+
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
         if (sender instanceof Player && args.length >= 1) {
@@ -23,5 +27,10 @@ public class JoinGameCommandExecutor implements CommandExecutor {
             return true;
         }
         return false;
+    }
+
+    @Override
+    public @Nullable List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String alias, @NotNull String[] args) {
+        return GameCache.getInstance().getGameNamesAsList();
     }
 }
