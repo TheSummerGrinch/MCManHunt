37b4d76c444f5cc5cd5b04711fb578490a0f7762
TheSummerGrinch <66258844+TheSummerGrinch@users.noreply.github.com>
2021-01-31 18:38:52 +0100

    Implementing TabCompleter
    Implemented a TabCompleter in SetDestroyUniberseOnStopCommandExecutor, to allow admins to easily user choose for which Universe to set the setting.

--- src/main/java/io/github/thesummergrinch/mcmanhunt/commands/game/op/universe/SetDestroyUniverseOnStopCommandExecutor.java
+++ src/main/java/io/github/thesummergrinch/mcmanhunt/commands/game/op/universe/SetDestroyUniverseOnStopCommandExecutor.java
@@ -5,9 +5,14 @@ import io.github.thesummergrinch.mcmanhunt.io.lang.LanguageFileLoader;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
-public class SetDestroyUniverseOnStopCommandExecutor implements CommandExecutor {
+import java.util.List;
+
+public class SetDestroyUniverseOnStopCommandExecutor implements CommandExecutor, TabCompleter {
+
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
         if (sender.isOp() && args.length >= 2 && UniverseCache.getInstance().getUniverse(args[0]) != null
@@ -17,4 +22,10 @@ public class SetDestroyUniverseOnStopCommandExecutor implements CommandExecutor 
             return true;
         } else return false;
     }
+
+    @Override
+    public @Nullable List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String alias, @NotNull String[] args) {
+        return UniverseCache.getInstance().getUniverseNamesAsList();
+    }
+
 }
