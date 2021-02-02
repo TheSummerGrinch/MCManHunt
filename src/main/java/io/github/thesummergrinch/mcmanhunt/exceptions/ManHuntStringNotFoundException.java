52888e2fc86494023291321bc9f412fffc054b57
TheSummerGrinch <66258844+TheSummerGrinch@users.noreply.github.com>
2021-02-02 23:40:59 +0100

    Delete ManHuntStringNotFoundException.java
    Unused exception

--- src/main/java/io/github/thesummergrinch/mcmanhunt/exceptions/ManHuntStringNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package io.github.thesummergrinch.mcmanhunt.exceptions;
-
-public class ManHuntStringNotFoundException extends Exception {
-
-    public ManHuntStringNotFoundException(final String message) {
-        super(message);
-    }
-
-}
