0bb8ba45fb2ff20f82bc5748bb990532f8b535ba
TheSummerGrinch <66258844+TheSummerGrinch@users.noreply.github.com>
2021-02-02 20:58:36 +0100

    Created LeaveGameCommandExecutor
    Will allow players to leave a game, should they need to.

---
+++ src/main/java/io/github/thesummergrinch/mcmanhunt/commands/game/player/LeaveGameCommandExecutor.java
@@ -0,0 +1,14 @@
+package io.github.thesummergrinch.mcmanhunt.commands.game.player;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.jetbrains.annotations.NotNull;
+
+//TODO implement
+public class LeaveGameCommandExecutor implements CommandExecutor {
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        return false;
+    }
+}
