de2ef697adc9c74895311b74e710e99ea363c9ee
TheSummerGrinch <66258844+TheSummerGrinch@users.noreply.github.com>
2021-02-03 20:00:13 +0100

    Prevent player from joining ongoing game
    This should prevent the player from joining a game that has already started. Also, we only show the standby games as autocomplete options.

--- src/main/java/io/github/thesummergrinch/mcmanhunt/commands/game/player/JoinGameCommandExecutor.java
+++ src/main/java/io/github/thesummergrinch/mcmanhunt/commands/game/player/JoinGameCommandExecutor.java
@@ -2,6 +2,7 @@ package io.github.thesummergrinch.mcmanhunt.commands.game.player;
 
 import io.github.thesummergrinch.mcmanhunt.cache.GameCache;
 import io.github.thesummergrinch.mcmanhunt.game.gamecontrols.Game;
+import io.github.thesummergrinch.mcmanhunt.game.gamecontrols.GameFlowState;
 import io.github.thesummergrinch.mcmanhunt.io.lang.LanguageFileLoader;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -19,8 +20,8 @@ public class JoinGameCommandExecutor implements CommandExecutor, TabCompleter {
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
         if (sender instanceof Player && args.length >= 1) {
-            Game game = GameCache.getInstance().getGameFromCache(args[0]);
-            if (game == null) return false;
+            final Game game = GameCache.getInstance().getGameFromCache(args[0]);
+            if (game == null || !game.getGameFlowState().equals(GameFlowState.DEFAULT)) return false;
             UUID playerUUID = ((Player) sender).getUniqueId();
             game.addPlayerToGame(playerUUID);
             sender.sendMessage(LanguageFileLoader.getInstance().getString("joined-game"));
@@ -31,6 +32,6 @@ public class JoinGameCommandExecutor implements CommandExecutor, TabCompleter {
 
     @Override
     public @Nullable List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String alias, @NotNull String[] args) {
-        return GameCache.getInstance().getGameNamesAsList();
+        return GameCache.getInstance().getStandbyGameNamesAsList();
     }
 }
