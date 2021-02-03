dc0c337c71aa3153180b25f23ee15719c956b5cf
TheSummerGrinch <66258844+TheSummerGrinch@users.noreply.github.com>
2021-02-03 21:09:24 +0100

    Improved Tracking Compass detection
    No longer using a hard-coded String. Rather using a customizable String, and a field that cannot be changed by the players.

--- src/main/java/io/github/thesummergrinch/mcmanhunt/eventhandlers/OnPlayerInteractEventHandler.java
+++ src/main/java/io/github/thesummergrinch/mcmanhunt/eventhandlers/OnPlayerInteractEventHandler.java
@@ -1,7 +1,11 @@
 package io.github.thesummergrinch.mcmanhunt.eventhandlers;
 
 import io.github.thesummergrinch.mcmanhunt.cache.CompassStateCache;
+import io.github.thesummergrinch.mcmanhunt.cache.PlayerStateCache;
+import io.github.thesummergrinch.mcmanhunt.game.players.PlayerRole;
+import io.github.thesummergrinch.mcmanhunt.game.players.PlayerState;
 import io.github.thesummergrinch.mcmanhunt.game.players.compasses.CompassState;
+import io.github.thesummergrinch.mcmanhunt.io.lang.LanguageFileLoader;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
@@ -20,19 +24,23 @@ public class OnPlayerInteractEventHandler implements Listener {
 
     @EventHandler(priority = EventPriority.HIGH)
     public void onPlayerInteractEvent(@NotNull final PlayerInteractEvent event) {
+        final PlayerState playerState = PlayerStateCache.getInstance().getPlayerState(event.getPlayer().getUniqueId());
+        if (!playerState.isInGame() || playerState.getPlayerRole().equals(PlayerRole.DEFAULT)) return;
         if (event.getAction().equals(Action.RIGHT_CLICK_AIR) || event.getAction().equals(Action.LEFT_CLICK_AIR)) {
             final Player sender = event.getPlayer();
             ItemStack compass;
             if (sender.getInventory().getItemInMainHand().getType().equals(Material.COMPASS)) {
                 compass = sender.getInventory().getItemInMainHand();
-                if (compass.getItemMeta().getDisplayName().contains(" Tracker")) {
-                    updateCompassMeta(sender, compass.getItemMeta().getDisplayName().split(" ")[0], (CompassMeta) compass.getItemMeta());
+                if (compass.getItemMeta().hasLore() && compass.getItemMeta().getLore().get(0)
+                        .equals(LanguageFileLoader.getInstance().getString("manhunt-compass"))) {
+                    updateCompassMeta(sender, compass.getItemMeta().getLore().get(1), (CompassMeta) compass.getItemMeta());
                 }
             }
             if (sender.getInventory().getItemInOffHand().getType().equals(Material.COMPASS)) {
                 compass = sender.getInventory().getItemInOffHand();
-                if (compass.getItemMeta().getDisplayName().contains(" Tracker")) {
-                    updateCompassMeta(sender, compass.getItemMeta().getDisplayName().split(" ")[0], (CompassMeta) compass.getItemMeta());
+                if (compass.getItemMeta().hasLore() && compass.getItemMeta().getLore().get(0)
+                        .equals(LanguageFileLoader.getInstance().getString("manhunt-compass"))) {
+                    updateCompassMeta(sender, compass.getItemMeta().getLore().get(1), (CompassMeta) compass.getItemMeta());
                 }
             }
         }
