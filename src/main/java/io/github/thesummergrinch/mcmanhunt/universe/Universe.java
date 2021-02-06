7dd639f81c36d4f481251f82cfc59e0a08298d78
TheSummerGrinch <66258844+TheSummerGrinch@users.noreply.github.com>
2021-02-06 17:52:48 +0100

    Universe.java no longer stores the actual World-objects, rather it stores their UUID. Also, changed formatting, somewhat.

--- src/main/java/io/github/thesummergrinch/mcmanhunt/universe/Universe.java
+++ src/main/java/io/github/thesummergrinch/mcmanhunt/universe/Universe.java
@@ -3,13 +3,9 @@ package io.github.thesummergrinch.mcmanhunt.universe;
 import io.github.thesummergrinch.mcmanhunt.MCManHunt;
 import io.github.thesummergrinch.mcmanhunt.cache.UniverseCache;
 import io.github.thesummergrinch.mcmanhunt.io.WorldDirectoryFileVisitor;
-import org.bukkit.Bukkit;
-import org.bukkit.Difficulty;
-import org.bukkit.GameRule;
-import org.bukkit.World;
-import org.bukkit.WorldCreator;
-import org.bukkit.WorldType;
+import org.bukkit.*;
 import org.bukkit.configuration.serialization.ConfigurationSerializable;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Flying;
 import org.bukkit.entity.Monster;
 import org.bukkit.scheduler.BukkitRunnable;
@@ -21,6 +17,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 import java.util.logging.Level;
 
 /**
@@ -29,62 +26,86 @@ import java.util.logging.Level;
  */
 public final class Universe implements ConfigurationSerializable {
 
-    // The Map<String, World> that effectively functions as the cache.
-    private final HashMap<String, World> worldHashMap;
+    // The Map<String, UUID> that effectively functions as the cache.
+    private final HashMap<String, UUID> worldHashMap;
 
     // Identifier used to register the Universe-object in the UniverseCache.
     private final String universeName;
 
-    // Toggleable flag used to denote whether or not the Universe, and the related persistent world-files should be
-    // deleted once the game ends (naturally or through commands). True by default, unless the game will be played in
-    // the default world.
+    // Toggleable flag used to denote whether or not the Universe, and the
+    // related persistent world-files should be deleted once the game ends
+    // (naturally or through commands). True by default, unless the game will be
+    // played in the default world.
     private boolean destroyWhenGameIsStopped;
 
-    // Flag used to determine whether or not the persistent world-files of this Universe should be deleted when the
-    // server is (gracefully) stopped.
+    // Flag used to determine whether or not the persistent world-files of this
+    // Universe should be deleted when the server is (gracefully) stopped.
     private boolean markedForDestruction;
 
     public Universe(final String universeName) {
+
         this.worldHashMap = new HashMap<>();
         this.universeName = universeName;
+
         populateUniverse();
         UniverseCache.getInstance().cacheUniverse(this.universeName, this);
+
         this.destroyWhenGameIsStopped = !universeName.equals("world");
         this.markedForDestruction = false;
+
     }
 
     /**
-     * Method solely used to deserialize a yml-representation of a {@link Universe}, as specified in the
-     * {@link #serialize}-method.
+     * Method solely used to deserialize a yml-representation of a
+     * {@link Universe}, as specified in the {@link #serialize}-method.
      *
-     * @param objects - Map of key-value pairs, representing the {@link Universe}-object.
+     * @param objects - Map of key-value pairs, representing the
+     * {@link Universe}-object.
      * @return universe - a reconstruction of the saved {@link Universe}.
      */
     @SuppressWarnings("unused")
     public static @NotNull Universe deserialize(Map<String, Object> objects) {
+
         Universe universe = new Universe((String) objects.get("name"));
-        universe.setDestroyWhenGameIsStopped((Boolean) objects.get("destroy-when-game-is-stopped"));
+
+        universe.setDestroyWhenGameIsStopped(
+                (Boolean) objects.get("destroy-when-game-is-stopped")
+        );
+
         return universe;
     }
 
     /**
-     * Generates an Overworld, a Nether and an End dimension, and stores them in the {@link HashMap}.
+     * Generates an Overworld, a Nether and an End dimension, and stores them
+     * in the {@link HashMap}.
      */
     private void populateUniverse() {
-        final World overworld = WorldCreator.name(universeName).environment(World.Environment.NORMAL)
+        final World overworld = WorldCreator.name(universeName)
+                .environment(World.Environment.NORMAL)
                 .generateStructures(true).type(WorldType.NORMAL).createWorld();
-        final World nether = WorldCreator.name(universeName + "_nether").environment(World.Environment.NETHER)
+
+        final World nether = WorldCreator.name(universeName + "_nether")
+                .environment(World.Environment.NETHER)
                 .generateStructures(true).type(WorldType.NORMAL).createWorld();
-        final World end = WorldCreator.name(universeName + "_the_end").environment(World.Environment.THE_END)
+
+        final World end = WorldCreator.name(universeName + "_the_end")
+                .environment(World.Environment.THE_END)
                 .generateStructures(true).type(WorldType.NORMAL).createWorld();
-        this.worldHashMap.put(overworld.getName(), overworld);
-        this.worldHashMap.put(nether.getName(), nether);
-        this.worldHashMap.put(end.getName(), end);
+
+        this.worldHashMap.put(overworld.getName(), overworld.getUID());
+        this.worldHashMap.put(nether.getName(), nether.getUID());
+        this.worldHashMap.put(end.getName(), end.getUID());
+
         if (MCManHunt.getPlugin(MCManHunt.class).getServer().getPluginManager()
                 .getPlugin("Multiverse-Core") != null
-                && MCManHunt.getPlugin(MCManHunt.class).getServer().getPluginManager()
+                && MCManHunt.getPlugin(MCManHunt.class).getServer()
+                .getPluginManager()
                 .getPlugin("Multiverse-NetherPortals") != null) {
-            MultiverseUniverseBridge.getInstance().registerWorldsInMultiverse(Bukkit.getConsoleSender(), this);
+
+            MultiverseUniverseBridge.getInstance()
+                    .registerWorldsInMultiverse
+                            (Bukkit.getConsoleSender(), this);
+
         }
     }
 
@@ -96,13 +117,14 @@ public final class Universe implements ConfigurationSerializable {
      */
     @Nullable
     public World getWorld(@NotNull final String worldName) {
-        return worldHashMap.get(worldName);
+        return Bukkit.getWorld(worldName);
     }
 
     /**
      * Serializes the {@link Universe}-object into a yml-representation.
      *
-     * @return {@link Map} containing key-value pairs representing the {@link Universe}.
+     * @return {@link Map} containing key-value pairs representing the
+     * {@link Universe}.
      */
     @Override
     public @NotNull Map<String, Object> serialize() {
@@ -115,41 +137,51 @@ public final class Universe implements ConfigurationSerializable {
     /**
      * Gets the name of the {@link Universe}.
      *
-     * @return
+     * @return - the universe name.
      */
     public @NotNull String getName() {
         return this.universeName;
     }
 
     /**
-     * Sets the {@link Difficulty} for each of the {@link World}-objects stored in the {@link Universe}.
+     * Sets the {@link Difficulty} for each of the {@link World}-objects stored
+     * in the {@link Universe}.
      *
      * @param difficulty - target difficulty
      */
     public void setDifficulty(@NotNull final Difficulty difficulty) {
-        this.worldHashMap.values().forEach(world -> world.setDifficulty(difficulty));
+        this.worldHashMap.values().forEach(worldUID
+            -> Bukkit.getWorld(worldUID).setDifficulty(difficulty));
     }
 
     /**
-     * Unloads all the {@link World}-objects stored in this {@link Universe}, and deletes the corresponding persistent
-     * world-files.
+     * Unloads all the {@link World}-objects stored in this {@link Universe},
+     * and deletes the corresponding persistent world-files.
      */
     public void destroyUniverse() {
-        this.worldHashMap.forEach((worldName, world) -> {
+
+        this.worldHashMap.keySet().forEach(worldName -> {
+
             Bukkit.unloadWorld(worldName, false);
+
             try {
+
                 File worldDirectory = new File(Bukkit.getWorldContainer(), "/" + worldName);
                 Files.walkFileTree(worldDirectory.toPath(), new WorldDirectoryFileVisitor());
+
             } catch (IOException exception) {
-                MCManHunt.getPlugin(MCManHunt.class).getLogger().log(Level.SEVERE, "Could not delete World "
-                        + worldName + ". Please check whether the world exists, " +
-                        "or contact the developer (GitHub: Github.com/TheSummerGrinch).");
+
+                MCManHunt.getPlugin(MCManHunt.class).getLogger().log(Level.SEVERE,
+                        "Could not delete World " + worldName + ". Please " +
+                                "check whether the world exists, or contact " +
+                                "the developer (GitHub: Github.com/TheSummerGrinch).");
             }
         });
     }
 
     /**
-     * @return boolean - whether or not the {@link Universe} should be permanently deleted when the game ends.
+     * @return boolean - whether or not the {@link Universe} should be
+     * permanently deleted when the game ends.
      */
     public boolean getDestroyWhenGameIsStopped() {
         return this.destroyWhenGameIsStopped;
@@ -169,15 +201,24 @@ public final class Universe implements ConfigurationSerializable {
     }
 
     public void unloadAndDestroy() {
+
         if (this.universeName.equals("world")) return;
-        this.worldHashMap.forEach((name, world) -> {
+
+        for (Map.Entry<String, UUID> entry : this.worldHashMap.entrySet()) {
+            String name = entry.getKey();
             Bukkit.unloadWorld(name, !markedForDestruction);
-        });
+        }
+
         if (!markedForDestruction) return;
 
-        if (MCManHunt.getPlugin(MCManHunt.class).getServer().getPluginManager().getPlugin("Multiverse-Core") != null) {
-            MultiverseUniverseBridge.getInstance().unloadAndDestroy(Bukkit.getConsoleSender(), this);
+        if (MCManHunt.getPlugin(MCManHunt.class).getServer().getPluginManager()
+                .getPlugin("Multiverse-Core") != null) {
+
+            MultiverseUniverseBridge.getInstance()
+                    .unloadAndDestroy(Bukkit.getConsoleSender(), this);
+
             UniverseCache.getInstance().removeUniverse(this.universeName);
+
             return;
         }
 
@@ -187,13 +228,20 @@ public final class Universe implements ConfigurationSerializable {
             new BukkitRunnable() {
                 @Override
                 public void run() {
+
                     if (!markedForDestruction) return;
+
                     try {
+
                         Files.walkFileTree(worldDirectory.toPath(), new WorldDirectoryFileVisitor());
+
                     } catch (IOException exception) {
-                        MCManHunt.getPlugin(MCManHunt.class).getLogger().log(Level.SEVERE, "Could not delete World "
-                                + worldName + ". Please check whether the world exists, " +
-                                "or contact the developer (GitHub: Github.com/TheSummerGrinch).");
+
+                        MCManHunt.getPlugin(MCManHunt.class).getLogger()
+                                .log(Level.SEVERE, "Could not delete World "
+                                    + worldName + ". Please check whether the world exists, " +
+                                    "or contact the developer (GitHub: Github.com/TheSummerGrinch).");
+
                     }
                 }
             }.runTaskAsynchronously(MCManHunt.getPlugin(MCManHunt.class));
@@ -205,7 +253,8 @@ public final class Universe implements ConfigurationSerializable {
 
     /**
      * Sets the specified {@link GameRule} to the specified value. E.g.
-     * {@code setGameRule(GameRule.DO_DAYLIGHT_CYCLE, false)} disables the daylight-cycle of the Worlds stored in this
+     * {@code setGameRule(GameRule.DO_DAYLIGHT_CYCLE, false)} disables the
+     * daylight-cycle of the Worlds stored in this
      * {@link Universe}.
      *
      * @param gameRule - The {@link GameRule} to be changed/set.
@@ -213,17 +262,24 @@ public final class Universe implements ConfigurationSerializable {
      * @param <T>      - Generic type
      */
     public <T> void setGameRule(GameRule<T> gameRule, T value) {
-        this.worldHashMap.values().forEach(world -> world.setGameRule(gameRule, value));
+        this.worldHashMap.values().forEach(worldUID
+                -> Bukkit.getWorld(worldUID).setGameRule(gameRule, value));
     }
 
     public void despawnMobs() {
-        this.worldHashMap.forEach((name, world) -> {
-            world.getEntities().forEach(entity -> {
+
+        for (Map.Entry<String, UUID> entry : this.worldHashMap.entrySet()) {
+
+            UUID worldUID = entry.getValue();
+
+            for (Entity entity : Bukkit.getWorld(worldUID).getEntities()) {
+
                 if (entity instanceof Monster || entity instanceof Flying) {
                     entity.remove();
                 }
-            });
-        });
+
+            }
+        }
     }
 
 }
