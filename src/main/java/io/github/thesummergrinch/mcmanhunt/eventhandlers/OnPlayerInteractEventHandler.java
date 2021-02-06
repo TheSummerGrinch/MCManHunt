3ef11f4ccf11b1b78ffb702662fdeabd3f441299
TheSummerGrinch <66258844+TheSummerGrinch@users.noreply.github.com>
2021-02-06 17:32:24 +0100

    Bugfix: Compass would allow players to keep tracking dead runners.
    Bugfix: Compass in off-hand wouldn't actually be updated, rather it would update the Compass in the main-hand twice.

--- src/main/java/io/github/thesummergrinch/mcmanhunt/eventhandlers/OnPlayerInteractEventHandler.java
+++ src/main/java/io/github/thesummergrinch/mcmanhunt/eventhandlers/OnPlayerInteractEventHandler.java
@@ -24,43 +24,111 @@ public class OnPlayerInteractEventHandler implements Listener {
 
     @EventHandler(priority = EventPriority.HIGH)
     public void onPlayerInteractEvent(@NotNull final PlayerInteractEvent event) {
-        final PlayerState playerState = PlayerStateCache.getInstance().getPlayerState(event.getPlayer().getUniqueId());
-        if (!playerState.isInGame() || playerState.getPlayerRole().equals(PlayerRole.DEFAULT)) return;
-        if (event.getAction().equals(Action.RIGHT_CLICK_AIR) || event.getAction().equals(Action.LEFT_CLICK_AIR)) {
+
+        final PlayerState playerState = PlayerStateCache.getInstance()
+        .getPlayerState(event.getPlayer().getUniqueId());
+
+        if (!playerState.isInGame()
+                || playerState.getPlayerRole().equals(PlayerRole.DEFAULT))
+            return;
+
+        if (event.getAction().equals(Action.RIGHT_CLICK_AIR)
+                || event.getAction().equals(Action.LEFT_CLICK_AIR)) {
+
             final Player sender = event.getPlayer();
             ItemStack compass;
-            if (sender.getInventory().getItemInMainHand().getType().equals(Material.COMPASS)) {
+
+            if (sender.getInventory().getItemInMainHand().getType()
+                .equals(Material.COMPASS)) {
+
                 compass = sender.getInventory().getItemInMainHand();
-                if (compass.getItemMeta().hasLore() && compass.getItemMeta().getLore().get(0)
-                        .equals(LanguageFileLoader.getInstance().getString("manhunt-compass"))) {
-                    updateCompassMeta(sender, compass.getItemMeta().getLore().get(1), (CompassMeta) compass.getItemMeta());
+
+                if (compass.getItemMeta().hasLore() && compass.getItemMeta()
+                .getLore().get(0).equals(LanguageFileLoader.getInstance()
+                .getString("manhunt-compass"))) {
+
+                    updateCompassMeta(sender,
+                            compass.getItemMeta().getLore().get(1),
+                            (CompassMeta) compass.getItemMeta(), Hand.MAIN_HAND);
+
                 }
             }
-            if (sender.getInventory().getItemInOffHand().getType().equals(Material.COMPASS)) {
+
+            if (sender.getInventory().getItemInOffHand().getType()
+                    .equals(Material.COMPASS)) {
+
                 compass = sender.getInventory().getItemInOffHand();
-                if (compass.getItemMeta().hasLore() && compass.getItemMeta().getLore().get(0)
-                        .equals(LanguageFileLoader.getInstance().getString("manhunt-compass"))) {
-                    updateCompassMeta(sender, compass.getItemMeta().getLore().get(1), (CompassMeta) compass.getItemMeta());
+
+                if (compass.getItemMeta().hasLore() && compass.getItemMeta()
+                        .getLore().get(0).equals(LanguageFileLoader.getInstance()
+                                .getString("manhunt-compass"))) {
+
+                    updateCompassMeta(sender,
+                            compass.getItemMeta().getLore().get(1),
+                            (CompassMeta) compass.getItemMeta(), Hand.OFF_HAND);
+
                 }
             }
         }
     }
 
     private @NotNull CompassState getUpdatedCompassMeta(@NotNull final UUID targetUUID, @NotNull CompassMeta compassMeta) {
+
         CompassState compassState = CompassStateCache.getInstance().getCompassState(targetUUID);
+
         if (compassState == null) {
+
             compassState = new CompassState(targetUUID, compassMeta);
+
         }
+
         return compassState;
     }
 
-    private void updateCompassMeta(@NotNull final Player sender, @NotNull final String targetName, @NotNull CompassMeta playerCompassMeta) {
+    private void updateCompassMeta(@NotNull final Player sender,
+                                   @NotNull final String targetName,
+                                   @NotNull final CompassMeta playerCompassMeta,
+                                   @NotNull final Hand handWithItem) {
+
         final Player target = Bukkit.getPlayer(targetName);
-        if (target != null) {
+
+        if (target ==  null) return;
+
+        final PlayerState playerState =
+                PlayerStateCache.getInstance().getPlayerState(target.getUniqueId());
+
+        if (playerState.isInGame() && !playerState.getPlayerRole().equals(PlayerRole.DEFAULT)) {
+
             CompassState compassState = getUpdatedCompassMeta(target.getUniqueId(), playerCompassMeta);
             CompassMeta compassMeta = compassState.getCompassMeta();
-            sender.getInventory().getItemInMainHand().setItemMeta(compassMeta);
+
+            if (handWithItem.equals(Hand.MAIN_HAND)) {
+
+                sender.getInventory().getItemInMainHand().setItemMeta(compassMeta);
+
+            } else {
+
+                sender.getInventory().getItemInOffHand().setItemMeta(compassMeta);
+
+            }
+
+        } else {
+
+            if (handWithItem.equals(Hand.MAIN_HAND)) {
+
+                sender.getInventory().setItemInMainHand(null);
+
+            } else {
+
+                sender.getInventory().setItemInOffHand(null);
+
+            }
         }
+    }
+
+    private enum Hand {
+        MAIN_HAND,
+        OFF_HAND
     }
 
 }
