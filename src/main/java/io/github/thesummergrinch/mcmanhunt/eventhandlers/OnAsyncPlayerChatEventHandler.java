629262cfe3d2b7e319d220cd415b87c7979e7557
TheSummerGrinch <66258844+TheSummerGrinch@users.noreply.github.com>
2021-02-06 17:23:23 +0100

    Bugfix: Messages would not arrive at teammates. This is due to the recipient-list in the AsyncPlayerChatEvent is (possibly) immutable. Now just sending the messages to the players through the server.

--- src/main/java/io/github/thesummergrinch/mcmanhunt/eventhandlers/OnAsyncPlayerChatEventHandler.java
+++ src/main/java/io/github/thesummergrinch/mcmanhunt/eventhandlers/OnAsyncPlayerChatEventHandler.java
@@ -14,7 +14,9 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.jetbrains.annotations.NotNull;
 
+import java.text.MessageFormat;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -23,36 +25,63 @@ public class OnAsyncPlayerChatEventHandler implements Listener {
     @EventHandler(priority = EventPriority.HIGHEST)
     public void onAsyncPlayerChatEvent(final AsyncPlayerChatEvent event) {
 
-        PlayerState playerState = PlayerStateCache.getInstance().getPlayerState(event.getPlayer().getUniqueId());
-        Game game = GameCache.getInstance().getGameFromCache(playerState.getGameName());
+        PlayerState playerState = PlayerStateCache.getInstance()
+                .getPlayerState(event.getPlayer().getUniqueId());
+
+        Game game = GameCache.getInstance()
+                .getGameFromCache(playerState.getGameName());
 
         if (!playerState.isInGame()
                 || game == null
                 || game.getGameFlowState().equals(GameFlowState.DEFAULT)
-                || playerState.getPlayerRole().equals(PlayerRole.DEFAULT)) return;
+                || playerState.getPlayerRole().equals(PlayerRole.DEFAULT))
+            return;
 
         if (playerState.getPlayerRole().equals(PlayerRole.RUNNER)) {
-            event.getRecipients().clear();
+
+            String format = ChatColor.DARK_GREEN + "[" + LanguageFileLoader
+                    .getInstance().getString("runner") + "]"
+                    + ChatColor.RESET +" <%s> %s";
+
+            format = MessageFormat.format(format, event.getPlayer().getName(),
+                    event.getMessage());
+
             Set<Player> recipients = new HashSet<>();
-            game.getRunners().stream().forEach(playerState1 -> {
-                recipients.add(Bukkit.getPlayer(playerState.getPlayerUUID()));
-            });
-            event.getRecipients().addAll(recipients);
-            String format = ChatColor.DARK_GREEN + "[" + LanguageFileLoader.getInstance().getString("runner") + "]" + ChatColor.RESET +" <%s> %s";
-            event.setFormat(format);
+
+            for (PlayerState runner : game.getRunners()) {
+                recipients.add(Bukkit.getPlayer(runner.getPlayerUUID()));
+            }
+
+            sendMessageToTeamMates(recipients, format);
 
         } else if (playerState.getPlayerRole().equals(PlayerRole.HUNTER)) {
-            event.getRecipients().clear();
+
+            String format = ChatColor.DARK_RED+ "[" + LanguageFileLoader
+                    .getInstance().getString("hunter") + "]"
+                            + ChatColor.RESET + " <%s> %s";
+
+            format = MessageFormat.format(format, event.getPlayer().getName(),
+                    event.getMessage());
+
             Set<Player> recipients = new HashSet<>();
-            game.getHunters().stream().forEach(playerState1 -> {
-                if (!playerState.isOnline()) return;
-                recipients.add(Bukkit.getPlayer(playerState.getPlayerUUID()));
-            });
-            event.getRecipients().addAll(recipients);
-            String format = ChatColor.DARK_RED+ "[" + LanguageFileLoader.getInstance().getString("hunter") + "]" + ChatColor.RESET + " <%s> %s";
-            event.setFormat(format);
+
+            for (PlayerState hunter : game.getHunters()) {
+                recipients.add(Bukkit.getPlayer(hunter.getPlayerUUID()));
+            }
+
+            sendMessageToTeamMates(recipients, format);
+
         }
 
+        event.setCancelled(true);
+
+    }
+
+    private void sendMessageToTeamMates(@NotNull final Set<Player> recipients,
+                                        @NotNull final String message) {
+        for (Player player : recipients) {
+            player.sendMessage(message);
+        }
     }
 
 }
