7300457eb1d73a65f26074c74f529def76d2bfc4
TheSummerGrinch <66258844+TheSummerGrinch@users.noreply.github.com>
2021-02-06 17:26:12 +0100

    Added 10-second delay to stopping the game after a win. Disabling the other team's win-event immediately, though.

--- src/main/java/io/github/thesummergrinch/mcmanhunt/eventhandlers/OnManHuntWinEventHandler.java
+++ src/main/java/io/github/thesummergrinch/mcmanhunt/eventhandlers/OnManHuntWinEventHandler.java
@@ -1,12 +1,15 @@
 package io.github.thesummergrinch.mcmanhunt.eventhandlers;
 
+import io.github.thesummergrinch.mcmanhunt.MCManHunt;
 import io.github.thesummergrinch.mcmanhunt.cache.GameCache;
 import io.github.thesummergrinch.mcmanhunt.events.ManHuntWinEvent;
 import io.github.thesummergrinch.mcmanhunt.io.lang.LanguageFileLoader;
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.scheduler.BukkitRunnable;
 import org.jetbrains.annotations.NotNull;
 
 public class OnManHuntWinEventHandler implements Listener {
@@ -14,11 +17,19 @@ public class OnManHuntWinEventHandler implements Listener {
     @EventHandler(priority = EventPriority.MONITOR)
     public void onManHuntWinEvent(@NotNull final ManHuntWinEvent event) {
         event.getWinners().forEach(uuid -> {
-            if (Bukkit.getPlayer(uuid) != null) {
+            Player player = Bukkit.getPlayer(uuid);
+            if (player != null && player.isOnline()) {
                 Bukkit.getPlayer(uuid).sendMessage(LanguageFileLoader.getInstance().getString("win-message"));
             }
         });
-        GameCache.getInstance().getGameFromCache(event.getGameName()).stop();
+        GameCache.getInstance().getGameFromCache(event.getGameName())
+                .getGameState().initializeTeamWin();
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                GameCache.getInstance().getGameFromCache(event.getGameName()).stop();
+            }
+        }.runTaskLater(MCManHunt.getPlugin(MCManHunt.class), 200L);
     }
 
 }
