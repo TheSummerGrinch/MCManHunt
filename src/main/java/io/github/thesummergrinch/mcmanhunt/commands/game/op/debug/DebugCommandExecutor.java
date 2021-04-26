ff5fcab40ed66b3122e7ba7447eb95c36ad79b8f
TheSummerGrinch <66258844+TheSummerGrinch@users.noreply.github.com>
2021-04-26 23:53:06 +0200

    Will be adding Debug functionality.

---
+++ src/main/java/io/github/thesummergrinch/mcmanhunt/commands/game/op/debug/DebugCommandExecutor.java
@@ -0,0 +1,29 @@
+package io.github.thesummergrinch.mcmanhunt.commands.game.op.debug;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
+public class DebugCommandExecutor implements CommandExecutor, TabCompleter {
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        if (sender.isOp() && sender instanceof Player) {
+            //TODO add Debug-functions
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public @Nullable List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String alias, @NotNull String[] args) {
+        return null;
+    }
+}
